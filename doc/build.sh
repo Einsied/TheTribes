3311a29f4fe5cd21faa7113efd24751fae885bbb
Einsied <43851735+Einsied@users.noreply.github.com>
2025-04-29 21:21:49 +0200

    Improving the build script
    I added a clean-up option so I know what is going on.

--- doc/build.sh
+++ doc/build.sh
@@ -11,11 +11,23 @@
 project_name="cultures_like"
 
 # Build to initialize everything
-pdflatex $project_name .tex
+pdflatex "$project_name".tex
 # Create the glossaries so we can reference them
-makeglossaries $project_name
+makeglossaries "$project_name"
 # Rebuild with the references
-pdflatex $project_name .tex
+pdflatex "$project_name".tex
 # Rebuild to fix the page-numbers moved by creating the table of contents
 # and the glossaries
-pdflatex $project_name .tex
+pdflatex "$project_name".tex
+
+# If a clean up is requested we clean up
+if [ "$1" = "-c" ]; then
+	rm "$project_name".aux
+	rm "$project_name".glg
+	rm "$project_name".glo
+	rm "$project_name".gls
+	rm "$project_name".ist
+	rm "$project_name".out
+	rm "$project_name".glo*
+	rm "$project_name".tpc
+fi
