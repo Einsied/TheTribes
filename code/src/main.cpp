28de81adb3c777f69c7a3657e1b81a1958f16ccf
Einsied <sebastian.einsiedler@noreply.com>
2025-05-23 21:39:20 +0200

    Improving cmake structure
    - We now have a dummy main
    - We have a module for the goods
    - The compiler warnings are active now

---
+++ code/src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief This is the main file running the game
+ *
+ * @author Sebastian Einsiedler
+ * @date 2025-05-23
+ * @copyright Copyright (c) 2025
+ */
+
+int main(int argument_count, char **argument_vector)
+{
+	return 0;
+};
