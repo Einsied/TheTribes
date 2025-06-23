5402428d0a1a88c818a3bcca62e8f3726da80959
Einsied <sebastian.einsiedler@noreply.com>
2025-06-23 21:52:24 +0200

    Moved the species and built it
    The base species is no built separatley

---
+++ code/src/inhabitants/species/base.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief This file defines the species class
+ *
+ * @author Sebastian Einsiedler
+ * @date 2025-06-08
+ * @copyright Copyright (c) 2025
+ */
+
+#include "tribes/inhabitants/species/base.hpp"
+
+namespace tribes::inhabitant::species{
+	Base::~Base() = default;
+};
